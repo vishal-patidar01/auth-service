910d4fb7d501ea5d802ec76285fde59bca0ad57e
vishal patidar <vishalsuman8770@gmail.com>
2026-02-07 03:24:41 +0530

    feat(auth-token): implement refresh token persistence

---
+++ src/main/java/com/vishal/auth/auth_app_backend/repositories/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.vishal.auth.auth_app_backend.repositories;
+
+import com.vishal.auth.auth_app_backend.entities.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+}
