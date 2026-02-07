31c307d19b27b40d6d9174f42bca3d920d07ccc9
vishal patidar <vishalsuman8770@gmail.com>
2026-02-08 00:04:42 +0530

    feat(auth-repo): add findByJti method to RefreshTokenRepository

--- src/main/java/com/vishal/auth/auth_app_backend/repositories/RefreshTokenRepository.java
+++ src/main/java/com/vishal/auth/auth_app_backend/repositories/RefreshTokenRepository.java
@@ -3,7 +3,10 @@ package com.vishal.auth.auth_app_backend.repositories;
 import com.vishal.auth.auth_app_backend.entities.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByJti(String jti);
 }
