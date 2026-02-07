fff85a14b95dbb4f84aecdd42daf36a051410f9a
vishal patidar <vishalsuman8770@gmail.com>
2026-02-08 00:00:07 +0530

    feat(security): allow public access to refresh and logout endpoints

--- src/main/java/com/vishal/auth/auth_app_backend/config/SecurityConfig.java
+++ src/main/java/com/vishal/auth/auth_app_backend/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig {
                          authorizeHttpRequests
                                 .requestMatchers("/api/v1/auth/register").permitAll()
                                 .requestMatchers("/api/v1/auth/login").permitAll()
+                                .requestMatchers("/api/v1/auth/refresh").permitAll()
+                                .requestMatchers("/api/v1/auth/logout").permitAll()
                                 .anyRequest().authenticated()
                 )
 //                .httpBasic(Customizer.withDefaults());
